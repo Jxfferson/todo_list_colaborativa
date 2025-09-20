f845c089ca54472a795d882424335b0ff4adbdb7
Jxfferson <jeffersonsticcorrealopez@gmail.com>
2025-09-20 11:22:32 -0500

    Cambio package.json

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        base: "todo_list_colaborativa",
+        globals: true,
+        environment: 'jsdom',
+    },
+})
