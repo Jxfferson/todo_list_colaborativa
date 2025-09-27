1efa71c4901af096d4988b2ab2e92ac95c961672
Jxfferson <jeffersonsticcorrealopez@gmail.com>
2025-09-27 08:55:16 -0500

    Despliego

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from 'react'
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
