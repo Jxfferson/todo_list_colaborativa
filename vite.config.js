9e74c959a5b87d8eab3b4f772d65eb6626fd390a
Jxfferson <jeffersonsticcorrealopez@gmail.com>
2025-09-27 08:25:59 -0500

    Despliego

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: "todo_list_colaborativa",
+    base: "/todo_list_colaborativa/",
     test: {
-        base: "todo_list_colaborativa",
+        base: "/todo_list_colaborativa/",
         globals: true,
         environment: 'jsdom',
     },
